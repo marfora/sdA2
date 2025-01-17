4d8efe0ebf0cf9efba5e0444c65b0b26306cf8be
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-01-17 11:45:01 +0100

    Add files via upload

---
+++ actividad 2/js/script.js
@@ -0,0 +1,11 @@
+function mostrarAlerta() {
+    alert("¡Hola!");
+    segundaFuncion();
+    }
+function mostrar(){
+    alert("¡Adios!");
+    segundaFuncion();
+}
+function segundaFuncion(){
+    alert(";-)")
+}
