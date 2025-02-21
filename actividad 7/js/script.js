e7e53981673ba3f88b2d77810b5118aaca159f7a
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-02-21 11:42:27 +0100

    Update script.js

--- actividad 7/js/script.js
+++ actividad 7/js/script.js
@@ -1,13 +1,13 @@
 function Comprobar() {
     let nota= ParseFloat(document.getElementById("nota").value;
-      let resultado= ParseFloat(document.getElementById("resultado");
+      let resultado= document.getElementById("resultado");
     if(nota < 0 || nota < 10){
     resultado.innerText = "El número que has puesto no es válido";
    }
      else if (nota < 5) {
       resultado.innerText = "Suspenso";
      }
-     if(nota < 9){
+     else{
    resultado.innerText = "Aprobado";
      }
 
