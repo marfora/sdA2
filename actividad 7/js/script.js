6d694ed61d6a47e0c1e37edb96398f015ede6d65
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-02-21 11:38:50 +0100

    Update script.js

--- actividad 7/js/script.js
+++ actividad 7/js/script.js
@@ -1,27 +1,14 @@
 function Comprobar() {
-    let nota= Parse
-    if(temp > 15 && temp < 30){
-    document.getElementById("parrafo1").innerHTML = "Hace Bueno";
-     document.getElementById("parrafo1").style.color = "green";
+    let nota= ParseFloat(document.getElementById("nota").value;
+      let resultado= ParseFloat(document.getElementById("resultado");
+    if(nota < 0 || nota < 10){
+    resultado.innerText = "El número que has puesto no es válido";
+   }
+     else if (nota < 5) {
+      resultado.innerText = "Suspenso";
      }
-     else{
-      document.getElementById("parrafo1").innerHTML = "Hace mal dia";
-     document.getElementById("parrafo1").style.color = "red";
+     if(nota < 9){
+   resultado.innerText = "Aprobado";
      }
-     if(temp < -40 || temp > 50){
-    document.getElementById("parrafo2").innerHTML = "voy a morir";
-     document.getElementById("parrafo2").style.color = "red";
-     }
-     else{
-      document.getElementById("parrafo2").innerHTML = "sobrevivire";
-     document.getElementById("parrafo2").style.color = "green";
-     }
-       if(!(temp >0)){
-    document.getElementById("parrafo3").innerHTML = "congelada";
-     document.getElementById("parrafo3").style.color = "red";
-     }
-     else{
-      document.getElementById("parrafo3").innerHTML = "liquita o gasioso";
-     document.getElementById("parrafo3").style.color = "green";
-     }
+
      }
