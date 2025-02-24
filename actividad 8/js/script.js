1c5bfee198b073d5f5a17b9a4df4e16c42a2c127
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-02-24 18:00:39 +0100

    Update script.js

--- actividad 8/js/script.js
+++ actividad 8/js/script.js
@@ -1,27 +1,25 @@
-function Comprobar() {
-    let calificar= Parse
-    if(temp > 15 && temp < 30){
-    document.getElementById("parrafo1").innerHTML = "Hace Bueno";
-     document.getElementById("parrafo1").style.color = "green";
-     }
-     else{
-      document.getElementById("parrafo1").innerHTML = "Hace mal dia";
-     document.getElementById("parrafo1").style.color = "red";
-     }
-     if(temp < -40 || temp > 50){
-    document.getElementById("parrafo2").innerHTML = "voy a morir";
-     document.getElementById("parrafo2").style.color = "red";
-     }
-     else{
-      document.getElementById("parrafo2").innerHTML = "sobrevivire";
-     document.getElementById("parrafo2").style.color = "green";
-     }
-       if(!(temp >0)){
-    document.getElementById("parrafo3").innerHTML = "congelada";
-     document.getElementById("parrafo3").style.color = "red";
-     }
-     else{
-      document.getElementById("parrafo3").innerHTML = "liquita o gasioso";
-     document.getElementById("parrafo3").style.color = "green";
-     }
-     }
+function MensajeSwitch() {  
+let hora = parseInt(prompt("Ingrese la hora (0-24):")); 
+if (hora < 0 || hora > 24) {  
+    alert("Error: La hora debe estar entre 0 y 24."); 
+return;  }  
+switch (true) {    
+case (hora >= 7 && hora <= 11):      
+alert("Buenos días!");      
+break;    
+case (hora >= 12 && hora <= 21):      
+alert("Buenas tardes!");      
+break;    
+default:      
+alert("Buenas noches!");  }}
+    
+function MensajeIf() {  
+let hora = parseInt(prompt("Ingrese la hora (0-24):"));  
+if (hora < 0 || hora > 24) {    
+alert("Error: La hora debe estar entre 0 y 24.");    
+return;  }  
+if (hora >= 7 && hora <= 11) {    
+alert("Buenos días!");  } 
+else if (hora >= 12 && hora <= 21) {    
+alert("Buenas tardes!");  } 
+else {    alert("Buenas noches!");  }}
