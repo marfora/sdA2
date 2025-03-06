9bf0bba27baf03ff414313b01a02101e739fb251
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-03-06 14:09:57 +0100

    Update script.js

--- actividad 7/js/script.js
+++ actividad 7/js/script.js
@@ -1,13 +1,14 @@
-function Comprobar() {
-    let nota= parseFloat(document.getElementById("nota").value);
-    let resultado= document.getElementById("resultado");
-    if(nota) || nota < 0 || nota < 10){
-   document.getElementById("resultado").innerHTML = "El número que has puesto no es válido";
-   }
-     else if (nota < 5) {
-     document.getElementById("resultado").innerHTML = "Suspenso";
+
+     function Comprobar() {
+      let nota= parseFloat(document.getElementById("nota").value);
+     
+      if(nota < 0 || nota > 10){
+     document.getElementById("resultado").innerHTML = "El número que has puesto no es válido";
      }
-     else{
- document.getElementById("resultado").innerHTML = "Aprobado";
-     }
-     }
+       else if (nota < 5) {
+       document.getElementById("resultado").innerHTML = "Suspenso";
+       }
+       else{
+   document.getElementById("resultado").innerHTML = "Aprobado";
+       }
+       }
