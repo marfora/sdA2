2f29e85e87f6f7f89e252b272fda8b70a0217835
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-03-07 11:55:56 +0100

    Add files via upload

--- actividad 9/js/script.js
+++ actividad 9/js/script.js
@@ -1,7 +1,40 @@
-function pedirDatos() {
-    let nombre;
-    let edad;
-    nombre =prompt(" ¿Cómo te llamas? ");
-    edad= prompt(" ¿Cuántos años tienes? ");
-    alert(" Hola " + nombre + " , veo que tienes " + edad + " años. ");
-}
+function MensajeSwitch() {  
+    let hora = parseInt(document.getElementById("hora").value, 10); 
+    
+    switch (hora){    
+    case 7 :   
+    case 8 : 
+    case 9 : 
+    case 10 : 
+    case 11 : 
+    case 12 : 
+        document.getElementById("resultado").innerHTML = "Buenos dias";      
+    break;
+    
+    case 13 :   
+    case 14 : 
+    case 15 : 
+    case 16 : 
+    case 17 : 
+    case 18 :
+    case 19 :
+    case 20 :
+    case 21 : 
+        document.getElementById("resultado").innerHTML = "Buenas tardes";
+    break;
+    
+    case 22 :
+    case 23 :
+    case 24 :
+    case 1 :
+    case 2 :
+    case 3 :
+    case 4 :
+    case 5 :
+    case 6 :
+        document.getElementById("resultado").innerHTML = "Buenas noches";
+    break;
+
+    default:
+        document.getElementById("resultado").innerHTML = "Esto no es una hora";
+    }}
