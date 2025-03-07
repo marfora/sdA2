fd4d7b05775ba5b1baeb888aa9d420809ecebba0
Mar Bella Fora Huarachi <mjorhua2608@g.educaand.es>
2025-03-07 22:57:53 +0100

    Update script.js

--- actividad 9/js/script.js
+++ actividad 9/js/script.js
@@ -1,40 +1,10 @@
-function MensajeSwitch() {  
-    let hora = parseInt(document.getElementById("hora").value, 10); 
-    
-    switch (hora){    
-    case 7 :   
-    case 8 : 
-    case 9 : 
-    case 10 : 
-    case 11 : 
-    case 12 : 
-        document.getElementById("resultado").innerHTML = "Buenos dias";      
-    break;
-    
-    case 13 :   
-    case 14 : 
-    case 15 : 
-    case 16 : 
-    case 17 : 
-    case 18 :
-    case 19 :
-    case 20 :
-    case 21 : 
-        document.getElementById("resultado").innerHTML = "Buenas tardes";
-    break;
-    
-    case 22 :
-    case 23 :
-    case 24 :
-    case 1 :
-    case 2 :
-    case 3 :
-    case 4 :
-    case 5 :
-    case 6 :
-        document.getElementById("resultado").innerHTML = "Buenas noches";
-    break;
-
-    default:
-        document.getElementById("resultado").innerHTML = "Esto no es una hora";
-    }}
+function Factorial() {  
+    let numero = parseInt(document.getElementById("numero").value, 10);    
+    let f= parseInt( 1 , 10 );
+    let i= parseInt( 0 , 10 );
+    while ( i < numero ){
+        i=i+1;
+        f=f*i;
+    }
+    document.getElementById("resultado").innerHTML = "El factorial de "+ numero + "es" + f;
+}
